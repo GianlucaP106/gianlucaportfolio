a027676647b2d05ec58c552485ed293cf35bbbf3
Gianluca Piccirillo <gianluca.piccirillo@mail.mcgill.ca>
2022-08-22 15:21:58 -0400

    cv

--- components/Layout/Navibar.js
+++ components/Layout/Navibar.js
@@ -24,9 +24,10 @@ export default function Navibar() {
                             <Nav.Link className="navLink myfont400" href="/">Home</Nav.Link>
                             <Nav.Link className="navLink myfont400" href="/experience" > Experience</Nav.Link>
                             <Nav.Link className="navLink myfont400" href="/skills" > Skills</Nav.Link>
+                            <Nav.Link className="navLink myfont400" href="/assets/Gianluca Piccirillo C.V. 2022.pdf" target={"_blank"} > Download my Resume</Nav.Link>
                         </Nav>
                         <Nav>
-                            <Nav.Link href="/contact"><button className="contactMeButton"><p style={{margin: "0"}} className="">Contact Me</p></button></Nav.Link>
+                            <Nav.Link style={{marginLeft: "10px"}} href="/contact"><button className="contactMeButton"><p style={{margin: "0"}} className="">Contact Me</p></button></Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
