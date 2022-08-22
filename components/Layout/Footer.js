79e3bcf363016f4f4c38fa4fb527622cfa985350
Gianluca Piccirillo <gianluca.piccirillo@mail.mcgill.ca>
2022-08-22 10:58:25 -0400

    some fixes

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Footer() {
@@ -10,6 +11,9 @@ export default function Footer() {
                     <Link href="/skills"><p className="navLink myfont40 footerLink">Skills</p></Link>
                 </div>
                 <Link href="/contact"><button className="contactMeButton footerButton"><p style={{margin: "0"}} className="">Contact Me</p></button></Link>
+                <div style={{marginTop: "20px", cursor: "pointer"}}>
+                    <Link href="/"><Image width={75} height={75} src="/assets/logos/G LOGO.png" /></Link>
+                </div>
             </div>
         </div>
     );
