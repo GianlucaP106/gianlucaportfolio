bb2b06b9eee4854ca67027a5f793fbbe0c4aab58
Gianluca Piccirillo <gianluca.piccirillo@mail.mcgill.ca>
2022-09-01 17:09:57 -0400

    footer change

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -5,13 +5,13 @@ export default function Footer() {
     return (
         <div className="footerBg">
             <div className="footerContainer">
-                <div className="footerContainer footerLinks">
+                {/* <div className="footerContainer footerLinks">
                     <Link href="/"><p className="navLink myfont40 footerLink">Home</p></Link>
                     <Link href="/experience"><p className="navLink myfont40 footerLink">Experience</p></Link>
                     <Link href="/skills"><p className="navLink myfont40 footerLink">Skills</p></Link>
                     <Link href="/assets/Gianluca Piccirillo C.V. 2022.pdf" target={"_blank"}><p className="navLink myfont40 footerLink">Download my Resume</p></Link>
-                </div>
-                <Link href="/contact"><button className="contactMeButton footerButton"><p style={{margin: "0"}} className="">Contact Me</p></button></Link>
+                </div> */}
+                {/* <Link href="/contact"><button className="contactMeButton footerButton"><p style={{margin: "0"}} className="">Contact Me</p></button></Link> */}
                 <div style={{marginTop: "20px", cursor: "pointer"}}>
                     <Link href="/"><Image width={75} height={75} src="/assets/logos/G LOGO.png" /></Link>
                 </div>
