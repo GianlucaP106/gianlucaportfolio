0e3e8e4e042fdbae03001e7f7fdc66b58483d209
gianlucapiccirillo <gianluca.piccirillo@mail.mcgill.ca>
2024-02-02 13:35:02 -0500

    fix: minor changes

--- components/ExperienceSection/ExperienceSection.tsx
+++ components/ExperienceSection/ExperienceSection.tsx
@@ -1,5 +1,4 @@
-import {experiences} from "@/components/ExperienceSection/experiences";
-import ExperienceCard from "@/components/ExperiencePopup/ExperienceCard";
+import { experiences } from "@/components/ExperienceSection/experiences";
 import ExperiencePopup from "@/components/ExperiencePopup/ExperiencePopup";
 
 export interface ExperienceSectionProps {
@@ -13,12 +12,12 @@ export default function ExperienceSection(props: ExperienceSectionProps) {
         <div className={"w-full text-center pt-20"}>
             <h2 className={"text-4xl text-white my-3"}>What is my relevant <span
                 className={"myTextGradient"}>Experience</span>?</h2>
-            <div className={"grid md:grid-cols-2 lg:grid-cols-3 gap-10 p-10"}>
+            <div className={"grid md:grid-cols-2 lg:grid-cols-3 gap-10 md:p-10"}>
                 {exps.map((exp, index) => {
                     return (
                         <div key={index} className={"w-full flex justify-center items-center px-8 sm:px-20 md:px-5"}>
                             <ExperiencePopup position={exp.title} company={exp.location} imageUrl={exp.image}
-                                            description={exp.description} type={exp.type} />
+                                description={exp.description} type={exp.type} />
                         </div>
                     )
                 })}
