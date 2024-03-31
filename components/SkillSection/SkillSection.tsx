bafbf6d4b16f3fd455c226b6410c45c68c8f04f6
Gianluca Piccirillo <gianluca.piccirillo@mail.mcgill.ca>
2024-03-31 18:54:41 -0400

    feat: modfied skill section

--- components/SkillSection/SkillSection.tsx
+++ components/SkillSection/SkillSection.tsx
@@ -5,7 +5,7 @@ import LogoSlider from "@/components/LogoSlider/LogoSlider";
 const skills: SkillCardProps[] = [
     {
         title: "Programming/Scripting Languages",
-        skills: ["Java", "Python", "TypeScript & JavaScript", "C", "C++", "ARM Assembly", "HTML/CSS", "SQL", "Bash & ZSH (Unix command lines)", "OCaml"],
+        skills: ["Java", "Python", "TypeScript & JavaScript", "C", "Rust", "ARM Assembly", "HTML/CSS", "SQL", "Bash & ZSH (Unix command lines)", "OCaml"],
     },
     {
         title: "Libraries and Frameworks",
@@ -13,7 +13,7 @@ const skills: SkillCardProps[] = [
     },
     {
         title: "Systems, Platforms And Tools",
-        skills: ["Github", "Git", "Microsoft Azure", "Firebase", "Google Cloud", "Docker", "VSCode", "IntelliJ", "NeoVim", "MacOS", "Linux", "PostgrSQL", "MySQL", "MongoDB", "Github Actions"] //, "Kubernetes fundamentals"]
+        skills: ["Github", "Git", "Microsoft Azure", "Firebase", "Google Cloud", "Docker", "VSCode", "IntelliJ", "NeoVim", "MacOS", "Linux", "PostgrSQL", "MySQL", "MongoDB", "Github Actions"]
     },
     {
         title: "Concepts and Other Technologies",
